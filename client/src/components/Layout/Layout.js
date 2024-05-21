63fb9a93915334d96884027700d76ecaf93331b6
Sanjeet kumar yadav <119010895+sanjeet-yadav@users.noreply.github.com>
2024-05-21 12:23:27 +0530

    creating SEO frendly setup

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import Header from "./Header"
 import Footer from "./Footer"
-const Layout = ({children}) =>{
+import {Helmet} from "react-helmet";
+const Layout = ({children,title,description,keywords,author}) =>{
   return (
     <div>
+      <Helmet>
+                <meta charSet="utf-8" />
+                <meta name="description" content={description}/>
+                <meta name="keywords" content= {keywords} />
+                <meta name="" content={author}/>
+                <title>{title}</title>
+            </Helmet>
       <Header/>
       <main style={{minHeight:'70vh'}}>{children}</main>
       <Footer/>
     </div>
   )
 }
+// defult descrption and keyords etc 
+Layout.defaultProps = {
+  title: 'Ecommerce app - shop now',
+  description: "mern stack project",
+  keywords: "mern,react,node,mongodb",
+  author: "Sanjeet",
+}
 
 export default Layout;
+
--- client/src/pages/Policy.js
+++ client/src/pages/Policy.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout/Layout'
 
 const Policy = () => {
   return (
-    <Layout>
+    <Layout  title={'Privacy Policy'}>
       <h1>Privecy Policy</h1>
     </Layout>
   )
